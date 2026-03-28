adb55ec2a53e64470050124363955a4533dafe82
Mina <bellella13@gmail.com>
2026-03-28 18:53:11 +0900

    fix: dev 모드에 핫리로드 할때마다 프리즈마 클라이언트 계속 생성되고 있었음

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,15 +1,24 @@
 import { PrismaClient } from "@/src/generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-});
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = new PrismaClient({
-  adapter,
-  log: [
-    { emit: "stdout", level: "query" },
-    { emit: "stdout", level: "error" },
-    { emit: "stdout", level: "warn" },
-  ],
-});
+function createPrismaClient() {
+  const adapter = new PrismaPg({
+    connectionString: process.env.DATABASE_URL,
+  });
+
+  return new PrismaClient({
+    adapter,
+    log:
+      process.env.NODE_ENV === "development"
+        ? [{ emit: "stdout", level: "error" }, { emit: "stdout", level: "warn" }]
+        : [],
+  });
+}
+
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
